5cd919c3f2b826932741a72d18e7ed1898b78328
lulu2002 <lose9749@gmail.com>
2020-02-06 19:24:28 +0800

    add another give method

--- src/main/java/org/mineacademy/fo/menu/tool/Tool.java
+++ src/main/java/org/mineacademy/fo/menu/tool/Tool.java
@@ -12,114 +12,123 @@ import org.mineacademy.fo.menu.model.ItemCreator;
  */
 public abstract class Tool {
 
-	/**
-	 * Create a new tool
-	 */
-	protected Tool() {
+    /**
+     * Create a new tool
+     */
+    protected Tool() {
 
-		// A hacky way of automatically registering it AFTER the parent constructor, assuming all went okay
-		new Thread() {
+        // A hacky way of automatically registering it AFTER the parent constructor, assuming all went okay
+        new Thread() {
 
-			@Override
-			public void run() {
+            @Override
+            public void run() {
 
-				try {
-					Thread.sleep(3);
-				} catch (final InterruptedException e) {
-					e.printStackTrace();
-				}
+                try {
+                    Thread.sleep(3);
+                } catch (final InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-				final Tool instance = Tool.this;
+                final Tool instance = Tool.this;
 
-				if (!ToolRegistry.isRegistered(instance))
-					ToolRegistry.register(instance);
-			}
-		}.start();
-	}
+                if (!ToolRegistry.isRegistered(instance))
+                    ToolRegistry.register(instance);
+            }
+        }.start();
+    }
 
-	/**
-	 * Evaluates the given itemstack whether it is this tool
-	 *
-	 * @param item the itemstack
-	 * @return true if this tool is the given itemstack
-	 */
-	public boolean isTool(ItemStack item) {
-		return ItemUtil.isSimilar(getItem(), item);
-	}
+    /**
+     * Evaluates the given itemstack whether it is this tool
+     *
+     * @param item the itemstack
+     * @return true if this tool is the given itemstack
+     */
+    public boolean isTool(ItemStack item) {
+        return ItemUtil.isSimilar(getItem(), item);
+    }
 
-	/**
-	 * Get the tool item
-	 *
-	 * TIP: Use {@link ItemCreator}
-	 *
-	 * @return the tool item
-	 */
-	public abstract ItemStack getItem();
+    /**
+     * Get the tool item
+     * <p>
+     * TIP: Use {@link ItemCreator}
+     *
+     * @return the tool item
+     */
+    public abstract ItemStack getItem();
 
-	/**
-	 * Called automatically when the tool is clicked
-	 *
-	 * @param event the event
-	 */
-	protected abstract void onBlockClick(PlayerInteractEvent event);
+    /**
+     * Called automatically when the tool is clicked
+     *
+     * @param event the event
+     */
+    protected abstract void onBlockClick(PlayerInteractEvent event);
 
-	/**
-	 * Called when the player swap items in their hotbar and the new slot matches
-	 * this tool.
-	 *
-	 * @param player the player
-	 */
-	protected void onHotbarFocused(Player player) {
-	}
+    /**
+     * Called when the player swap items in their hotbar and the new slot matches
+     * this tool.
+     *
+     * @param player the player
+     */
+    protected void onHotbarFocused(Player player) {
+    }
 
-	/**
-	 * Called when the player the tool is out of focus at hotbar
-	 *
-	 * @param player the player
-	 */
-	protected void onHotbarDefocused(Player player) {
-	}
+    /**
+     * Called when the player the tool is out of focus at hotbar
+     *
+     * @param player the player
+     */
+    protected void onHotbarDefocused(Player player) {
+    }
 
-	/**
-	 * Should we fire {@link #onBlockClick(PlayerInteractEvent)} even on cancelled
-	 * events?
-	 *
-	 * True by default. Set to false if you want to catch clicking air.
-	 *
-	 * @return true if we should ignore the click event if it was cancelled
-	 */
-	protected boolean ignoreCancelled() {
-		return true;
-	}
+    /**
+     * Should we fire {@link #onBlockClick(PlayerInteractEvent)} even on cancelled
+     * events?
+     * <p>
+     * True by default. Set to false if you want to catch clicking air.
+     *
+     * @return true if we should ignore the click event if it was cancelled
+     */
+    protected boolean ignoreCancelled() {
+        return true;
+    }
 
-	/**
-	 * A convenience method, should we automatically cancel the
-	 * {@link PlayerInteractEvent} ?
-	 *
-	 * @return true if the interact event should be cancelled automatically false by
-	 *         default
-	 */
-	protected boolean autoCancel() {
-		return false;
-	}
+    /**
+     * A convenience method, should we automatically cancel the
+     * {@link PlayerInteractEvent} ?
+     *
+     * @return true if the interact event should be cancelled automatically false by
+     * default
+     */
+    protected boolean autoCancel() {
+        return false;
+    }
 
-	/**
-	 * Convenience method for quickly adding this tool into a players inventory
-	 *
-	 * @param player
-	 */
-	public final void give(Player player) {
-		player.getInventory().addItem(getItem());
-	}
+    /**
+     * Convenience method for quickly setting this tool to a specific slot of players inventory
+     *
+     * @param player
+     */
+    public final void give(Player player, int slot) {
+        player.getInventory().setItem(slot, getItem());
+    }
 
-	/**
-	 * Returns true if the compared object is a tool with the same {@link #getItem()}
-	 *
-	 * @param obj
-	 * @return
-	 */
-	@Override
-	public final boolean equals(Object obj) {
-		return obj instanceof Tool && ((Tool) obj).getItem().equals(getItem());
-	}
+    /**
+     * Convenience method for quickly adding this tool into a players inventory
+     *
+     * @param player
+     */
+    public final void give(Player player) {
+        player.getInventory().addItem(getItem());
+    }
+
+    /**
+     * Returns true if the compared object is a tool with the same {@link #getItem()}
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public final boolean equals(Object obj) {
+        return obj instanceof Tool && ((Tool) obj).getItem().equals(getItem());
+    }
 }
