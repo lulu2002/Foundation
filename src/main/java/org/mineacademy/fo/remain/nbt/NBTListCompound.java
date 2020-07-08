b49e6666830f5a362804df1a4e261795ba029139
Leonhard <javafactorydev@gmail.com>
2020-07-08 15:05:39 +0200

    Urgent: Update NBT

--- src/main/java/org/mineacademy/fo/remain/nbt/NBTListCompound.java
+++ src/main/java/org/mineacademy/fo/remain/nbt/NBTListCompound.java
@@ -7,36 +7,35 @@ package org.mineacademy.fo.remain.nbt;
  * future
  *
  * @author tr7zw
- *
  */
 public class NBTListCompound extends NBTCompound {
 
-	private final NBTList<?> owner;
-	private Object compound;
+    private NBTList<?> owner;
+    private Object compound;
 
-	protected NBTListCompound(NBTList<?> parent, Object obj) {
-		super(null, null);
-		owner = parent;
-		compound = obj;
-	}
+    NBTListCompound(final NBTList<?> parent, final Object obj) {
+        super(null, null);
+        owner = parent;
+        compound = obj;
+    }
 
-	public NBTList<?> getListParent() {
-		return owner;
-	}
+    public NBTList<?> getListParent() {
+        return owner;
+    }
 
-	@Override
-	public Object getCompound() {
-		return compound;
-	}
+    @Override
+    public Object getCompound() {
+        return compound;
+    }
 
-	@Override
-	protected void setCompound(Object compound) {
-		this.compound = compound;
-	}
+    @Override
+    protected void setCompound(final Object compound) {
+        this.compound = compound;
+    }
 
-	@Override
-	protected void saveCompound() {
-		owner.save();
-	}
+    @Override
+    protected void saveCompound() {
+        owner.save();
+    }
 
 }
--- src/main/java/org/mineacademy/fo/remain/nbt/NBTLongList.java
+++ src/main/java/org/mineacademy/fo/remain/nbt/NBTLongList.java
@@ -8,34 +8,37 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 /**
- * String implementation for NBTLists
+ * Long implementation for NBTLists
  *
  * @author tr7zw
  */
-public class NBTStringList extends NBTList<String> {
+public class NBTLongList extends NBTList<Long> {
 
-    NBTStringList(final NBTCompound owner, final String name, final NBTType type, final Object list) {
+    NBTLongList(final NBTCompound owner, final String name, final NBTType type, final Object list) {
         super(owner, name, type, list);
     }
 
     @Override
-    public String get(final int index) {
+    protected Object asTag(final Long object) {
         try {
-            return (String) ReflectionMethod.LIST_GET_STRING.run(listObject, index);
-        } catch (final Exception ex) {
-            throw new NbtApiException(ex);
-        }
-    }
-
-    @Override
-    protected Object asTag(final String object) {
-        try {
-            final Constructor<?> con = ClassWrapper.NMS_NBTTAGSTRING.getClazz().getDeclaredConstructor(String.class);
+            final Constructor<?> con = ClassWrapper.NMS_NBTTAGLONG.getClazz().getDeclaredConstructor(long.class);
             con.setAccessible(true);
             return con.newInstance(object);
         } catch (final InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
                 | NoSuchMethodException | SecurityException e) {
             throw new NbtApiException("Error while wrapping the Object " + object + " to it's NMS object!", e);
+        }
+    }
+
+    @Override
+    public Long get(final int index) {
+        try {
+            final Object obj = ReflectionMethod.LIST_GET.run(listObject, index);
+            return Long.valueOf(obj.toString().replace("L", ""));
+        } catch (final NumberFormatException nf) {
+            return 0l;
+        } catch (final Exception ex) {
+            throw new NbtApiException(ex);
         }
     }
 
