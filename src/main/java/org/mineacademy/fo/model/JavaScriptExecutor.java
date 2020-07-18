fce0470605e15c1f7e3306d0b3278f2163f82be0
Leonhard <javafactorydev@gmail.com>
2020-07-18 20:54:21 +0200

    Improve JavaScriptExecutor to fix issues.

--- src/main/java/org/mineacademy/fo/model/JavaScriptExecutor.java
+++ src/main/java/org/mineacademy/fo/model/JavaScriptExecutor.java
@@ -7,7 +7,6 @@ import org.bukkit.event.Event;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.collection.expiringmap.ExpiringMap;
 import org.mineacademy.fo.plugin.SimplePlugin;
-import org.mineacademy.fo.settings.SimpleSettings;
 
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
@@ -21,43 +20,38 @@ import java.util.concurrent.TimeUnit;
 /**
  * An engine that compiles and executes code on the fly.
  * <p>
- * The code is based off JavaScript with new Java methods, see: https://winterbe.com/posts/2014/04/05/java8-nashorn-tutorial/
+ * The code is based off JavaScript with new Java methods, see:
+ * https://winterbe.com/posts/2014/04/05/java8-nashorn-tutorial/
  */
 public final class JavaScriptExecutor {
 
 	/**
 	 * The engine singleton
 	 */
-	private static ScriptEngine engine = null;
+	private static final ScriptEngine engine;
 
 	/**
 	 * Cache scripts for 1 second per player for highest performance
 	 * <p>
 	 * Player -> Map of scripts and their results
 	 */
-	private static final Map<UUID, Map<String, Object>> resultCache = ExpiringMap
-		.builder()
-		.expiration(1, TimeUnit.SECONDS)
-		.build();
+	private static final Map<UUID, Map<String, Object>> resultCache = ExpiringMap.builder().expiration(1, TimeUnit.SECONDS).build();
 
 	// Load the engine
 	static {
-		if (SimpleSettings.USE_JAVA_SCRIPT_ENGINE) {
-			Thread.currentThread().setContextClassLoader(SimplePlugin.class.getClassLoader());
+		Thread.currentThread().setContextClassLoader(SimplePlugin.class.getClassLoader());
 
-			final ScriptEngineManager engineManager = new ScriptEngineManager(null);
-			engine = engineManager.getEngineByName("Nashorn");
+		final ScriptEngineManager engineManager = new ScriptEngineManager(null);
+		engine = engineManager.getEngineByName("Nashorn");
 
-			if (engine == null)
-				Common.logFramed(
-					true,
-					"JavaScript placeholders will not function!",
-					"",
-					"Your Java version/distribution lacks",
-					"the Nashorn library for JavaScript",
-					"placeholders. Ensure you have Oracle",
-					"Java 8.");
-		}
+		if (engine == null)
+			Common.logFramed(true,
+				"JavaScript placeholders will not function!",
+				"",
+				"Your Java version/distribution lacks",
+				"the Nashorn library for JavaScript",
+				"placeholders. Ensure you have Oracle",
+				"Java 8.");
 	}
 
 	/**
@@ -71,7 +65,8 @@ public final class JavaScriptExecutor {
 	}
 
 	/**
-	 * Runs the given JavaScript code for the player, making the "player" variable in the code usable
+	 * Runs the given JavaScript code for the player,
+	 * making the "player" variable in the code usable
 	 *
 	 * @param javascript
 	 * @param sender
@@ -82,8 +77,8 @@ public final class JavaScriptExecutor {
 	}
 
 	/**
-	 * Compiles and executes the Javascript code for the player ("player" variable is put into the JS
-	 * code) as well as the bukkit event (use "event" variable there)
+	 * Compiles and executes the Javascript code for the player ("player" variable is put into the JS code)
+	 * as well as the bukkit event (use "event" variable there)
 	 *
 	 * @param javascript
 	 * @param sender
@@ -91,49 +86,45 @@ public final class JavaScriptExecutor {
 	 * @return
 	 */
 	public static Object run(@NonNull String javascript, CommandSender sender, Event event) {
-		synchronized (engine) {
-			// Cache for highest performance
-			Map<String, Object> cached = sender instanceof Player
-				? resultCache.get(((Player) sender).getUniqueId())
-				: null;
 
-			if (cached != null) {
-				final Object result = cached.get(javascript);
+		// Cache for highest performance
+		Map<String, Object> cached = sender instanceof Player ? resultCache.get(((Player) sender).getUniqueId()) : null;
 
-				if (result != null)
-					return result;
-			}
+		if (cached != null) {
+			final Object result = cached.get(javascript);
 
-			try {
-				engine.getBindings(ScriptContext.ENGINE_SCOPE).clear();
-
-				if (sender != null)
-					engine.put("player", sender);
-
-				if (event != null)
-					engine.put("event", event);
-
-				final Object result = engine.eval(javascript);
-
-				if (sender instanceof Player) {
-					if (cached == null)
-						cached = new HashMap<>();
-
-					cached.put(javascript, result);
-					resultCache.put(((Player) sender).getUniqueId(), cached);
-				}
-
+			if (result != null)
 				return result;
+		}
 
-			} catch (final ScriptException ex) {
-				Common.error(
-					ex,
-					"Script executing failed!",
-					"Script: " + javascript,
-					"%error");
+		try {
+			engine.getBindings(ScriptContext.ENGINE_SCOPE).clear();
 
-				return null;
+			if (sender != null)
+				engine.put("player", sender);
+
+			if (event != null)
+				engine.put("event", event);
+
+			final Object result = engine.eval(javascript);
+
+			if (sender instanceof Player) {
+				if (cached == null)
+					cached = new HashMap<>();
+
+				cached.put(javascript, result);
+				resultCache.put(((Player) sender).getUniqueId(), cached);
 			}
+
+			return result;
+
+		} catch (final ScriptException ex) {
+			Common.error(ex,
+				"Script executing failed!",
+				"Script: " + javascript,
+				"%error");
+
+			return null;
 		}
 	}
 
@@ -145,16 +136,13 @@ public final class JavaScriptExecutor {
 	 * @return
 	 * @throws ScriptException
 	 */
-	public static Object run(String javascript, Map<String, Object> replacements)
-		throws ScriptException {
-		synchronized (engine) {
-			engine.getBindings(ScriptContext.ENGINE_SCOPE).clear();
+	public static Object run(String javascript, Map<String, Object> replacements) throws ScriptException {
+		engine.getBindings(ScriptContext.ENGINE_SCOPE).clear();
 
-			if (replacements != null)
-				for (final Map.Entry<String, Object> replacement : replacements.entrySet())
-					engine.put(replacement.getKey(), replacement.getValue());
+		if (replacements != null)
+			for (final Map.Entry<String, Object> replacement : replacements.entrySet())
+				engine.put(replacement.getKey(), replacement.getValue());
 
-			return engine.eval(javascript);
-		}
+		return engine.eval(javascript);
 	}
 }
